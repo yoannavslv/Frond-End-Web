ea08c670f7502375779cfddc4ff2b221937004e1
Yoanna Vasileva <61412093+yoannavslv@users.noreply.github.com>
2020-11-22 12:20:00 +0200

    Update task8.js

--- Fourth_Week/task8.js
+++ Fourth_Week/task8.js
@@ -9,7 +9,7 @@ class CashMethod extends PaymentMethod {
         super();
         let amountV = amount;
 
-        this.addtoAmount = (new_amount) => {
+        this.addToAmount = (new_amount) => {
             amountV += new_amount;
             return this;
         };
@@ -31,7 +31,7 @@ class CreditMethod extends PaymentMethod {
         super();
         let amountV = amount;
 
-        this.addtoAmount = (new_amount) => {
+        this.addToAmount = (new_amount) => {
             amountV += 0.9 * new_amount;
             return this;
         };
@@ -46,13 +46,3 @@ class CreditMethod extends PaymentMethod {
         };
     }
 }
-
-const cashAccount = new CashMethod();
-		cashAccount.getAmount(); // returns “Your amount in the account is: 0”
-		cashAccount.addToAmount(100); // returns cashAccount instance (useful for method chaining)
-		cashAccount.addToAmount(20).addToAmount(30).reduceFromAmount(10); // returns cashAccount instance (useful for method chaining)
-		cashAccount.getAmount(); // returns “Your amount in the account is: 140”
-
-		const creditAccount = new CreditMethod();
-		creditAccount.addToAmount(100); // returns creditAccount instance (useful for method chaining)
-		creditAccount.getAmount(); // returns “Your amount in the account is: 90”;
